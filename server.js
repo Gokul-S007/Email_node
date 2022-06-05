6d695850c6332a1240ee12464f0029c14ab5c3fb
TJ658 <gokulmani682001@gmail.com>
2022-06-05 23:23:29 +0530

    updated

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const app = express();
+
+const nodemailer = require("nodemailer");
+
+dotenv.config({ path: "config.env" });
+const PORT = process.env.PORT || 5000;
+
+// middleware
+app.use(express.static("public"));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/contactform.html");
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+
+  const transporter = nodemailer.createTransport({
+    service:'gmail',
+    auth : {
+      user:'gokulmani682004@gmail.com',
+      pass:'12345@#&'
+    }
+  })
+
+  const mailOptions = {
+    from:req.body.email,
+    to:'gokulmani682004',
+    subject:`Message from ${req.body.email}: ${req.body.subjetc}`,
+    text:req.body.message
+  }
+
+  transporter.sendMail(mailOptions,(error,info)=>{
+    if(error){
+      console.log(error);
+      res.send(error);
+    }
+    else{
+      console.log('Email sent :' + info.response);
+      res.send('success');
+    }
+  })
+
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at Port ${PORT}`);
+});
